3c4b1920d87d5e6fd74e52b72f678286bad47978
puski96 <72152563+puski96@users.noreply.github.com>
2021-07-08 16:44:48 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import React, { Component }  from 'react';
 import "./App.css";
 import MainPageComponent from "./components/MainPageComponent";
 import HeaderComponent from "./components/HeaderComponent";
